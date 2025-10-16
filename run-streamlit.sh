88095e9facbe8a4ba40bfb1a6a78b983f9013bc3
Eita Kunieda <sound013italy@gmail.com>
2025-10-16 20:11:02 +0800

    Update run-streamlit.sh

--- run-streamlit.sh
+++ run-streamlit.sh
@@ -1,11 +1,15 @@
-sudo apt-get update
-sudo apt-get install -y libicu-dev
+#sudo apt-get update
+#sudo apt-get install -y libicu-dev
 
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y icu-devtools libicu-dev libicu70
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
 #!/bin/bash
 # 環境変数を追加
 #export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=true
-export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+#export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
 # Streamlit 起動
 #sudo -E -u appuser /home/adminuser/venv/bin/streamlit run app.py
