85d70712f64097dbe921aeec5f9d57c4bdb33bf7
Eita Kunieda <sound013italy@gmail.com>
2025-10-16 19:34:04 +0800

    Update run-streamlit.sh

--- run-streamlit.sh
+++ run-streamlit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 環境変数を追加
-export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=true
+#export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=true
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
 # Streamlit 起動
 sudo -E -u appuser /home/adminuser/venv/bin/streamlit run app.py
